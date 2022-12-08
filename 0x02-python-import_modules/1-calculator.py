83b26617c2f64f575e8168d6d0b0c2321516f804
Modasoka <kaytujohn254@gmail.com>
2022-12-08 03:53:49 +0300

    program that imports add() function & prints result of the addition

--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-print(add(a, b))
-print(sub(a, b))
-print(mul(a, b))
-print(div(a, b))
+    a = 10
+    b = 5
+
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))

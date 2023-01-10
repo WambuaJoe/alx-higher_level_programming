bcb5dd12c80e3d47e6dfbf6c0e29c116c0a39ccc
Modasoka <kaytujohn254@gmail.com>
2023-01-10 11:37:55 +0300

    write a class based on another class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" defines a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ represents BaseGeometry """
+    def area(self):
+        """ raises an exception """
+        raise Exception("area() has not been implemented")

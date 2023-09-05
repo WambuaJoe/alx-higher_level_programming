b69d8d446c3c9b6dae89318849a0b3e295e88174
WambuaJoe <kaytujohn254@gmail.com>
2023-09-05 14:36:59 +0300

    code update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,4 +2,4 @@
 import sys
 message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(message)
-sys.stderr.flush()
+quit(1)
--- 0x00-python-hello_world/main.py
+++ 0x00-python-hello_world/main.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(f"Best School: {88+10}")
+print("Best School")
+

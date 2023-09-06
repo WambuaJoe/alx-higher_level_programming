6ec3690b78aa5b2bcf5216a7d7a2fd39734809a7
WambuaJoe <kaytujohn254@gmail.com>
2023-09-06 22:59:37 +0300

    code fix - string fromatter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for char in str:
         if 123 > ord(char) >= 97:
             char = chr(ord(char) - 32)
-        print(f"{char}", end='')
+        print("{}".format(char), end='')
     print()

dbf5e7fe7788e44c5d70d7f29c3534149d508388
WambuaJoe <kaytujohn254@gmail.com>
2023-09-13 00:02:08 +0300

    code fix - linter check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("{:d} argument:".format(counter - 1))
     else:
         print("{:d} arguments:".format(counter - 1))
-    
+
     for elm in range(1, counter):
         print("{:d}: {:s}".format((elm), argv[elm]))

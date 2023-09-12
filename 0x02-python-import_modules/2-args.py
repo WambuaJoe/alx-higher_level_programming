a468f9c3da647cd137cd8aa67203784257d2a750
WambuaJoe <kaytujohn254@gmail.com>
2023-09-12 18:07:35 +0300

    code fix - use sys module to print arg count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv[1:]) == 0:
-        print("{} arguments".format(len(sys.argv[1:])))
-    elif len(sys.argv[1:]) == 1:
-        print("{} argument:".format(len(sys.argv[1:])))
-        for i in range(1, len(sys.argv[0:])):
-            print("{}: {}".format(i, sys.argv[i]))
+    from sys import argv
+
+    # while(len(argv) > 1):
+        # if(len(argv) == 2):
+            # print("{} argument:".format(len(argv) - 1))
+            # print("{}: {}".format(len(argv) - 1, argv[1]))
+            # exit()
+        # else:
+            # print("{} arguments:".format(len(argv) - 1))
+            # for i in range(1, len(argv[0:])):
+                # print("{}: {}".format(i, argv[i]))
+                # exit()
+    # print("0 arguments.")
+
+    if len(argv[1:]) == 0:
+        print("{} arguments".format(len(argv[1:])))
+    elif len(argv[1:]) == 1:
+        print("{} argument:".format(len(argv[1:])))
+        for i in range(1, len(argv[0:])):
+            print("{}: {}".format(i, argv[i]))
     else:
-        print(("{} arguments:".format(len(sys.argv[1:]))))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print(("{} arguments:".format(len(argv[1:]))))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))

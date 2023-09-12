b3427a7a5dba209f04c2bb78d3592a8cb443a5bf
WambuaJoe <kaytujohn254@gmail.com>
2023-09-12 23:50:12 +0300

    code fix - linter check

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for a in range(4, 6):
             c = add(c, a)
-        return(c)
+        return (c)
     else:
-        return(sub(a, b))
+        return (sub(a, b))

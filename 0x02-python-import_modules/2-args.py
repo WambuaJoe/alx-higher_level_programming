b63ace7219d33f00843c67cd9843aaaec6cc8e74
WambuaJoe <kaytujohn254@gmail.com>
2023-09-12 23:58:01 +0300

    code fix - use sys module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,25 +2,13 @@
 if __name__ == "__main__":
     from sys import argv
 
-    # while(len(argv) > 1):
-        # if(len(argv) == 2):
-            # print("{} argument:".format(len(argv) - 1))
-            # print("{}: {}".format(len(argv) - 1, argv[1]))
-            # exit()
-        # else:
-            # print("{} arguments:".format(len(argv) - 1))
-            # for i in range(1, len(argv[0:])):
-                # print("{}: {}".format(i, argv[i]))
-                # exit()
-    # print("0 arguments.")
-
-    if len(argv[1:]) == 0:
-        print("{} arguments".format(len(argv[1:])))
-    elif len(argv[1:]) == 1:
-        print("{} argument:".format(len(argv[1:])))
-        for i in range(1, len(argv[0:])):
-            print("{}: {}".format(i, argv[i]))
+    counter = len(argv)
+    if counter == 1:
+        print("0 arguments.")
+    elif counter == 2:
+        print("{:d} argument:".format(counter - 1))
     else:
-        print(("{} arguments:".format(len(argv[1:]))))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{:d} arguments:".format(counter - 1))
+    
+    for elm in range(1, counter):
+        print("{:d}: {:s}".format((elm), argv[elm]))

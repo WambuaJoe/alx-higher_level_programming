368da74a4b13c2bae90a743677296298343ab53c
WambuaJoe <kaytujohn254@gmail.com>
2023-10-03 21:28:22 +0300

    code fix - documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Square class to represent a square"""
+
 
 class Square:
-	""" define a class Square """
-	pass
+    """
+    Defines a Square and its basic properties
+    """
+    pass

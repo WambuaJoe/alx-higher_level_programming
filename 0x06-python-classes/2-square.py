12377697b75078cf4243edff4a439f825d8b3399
WambuaJoe <kaytujohn254@gmail.com>
2023-10-03 22:16:05 +0300

    implement exception handling

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Square class to represent a class """
+
+
+class Square:
+    """
+    defines a Square & its basic properties
+    """
+    def __init__(self, size=0):
+        try:

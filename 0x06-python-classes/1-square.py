966dd826d4109e047c8c011b0552d26ee202b807
WambuaJoe <kaytujohn254@gmail.com>
2023-10-03 21:40:17 +0300

    create class with pricate instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Square class to represent a square """
+
+
+class Square:
+    """
+    defines a Square & its basic properties
+    """
+    def __init__(self, size):
+        self.__size = size

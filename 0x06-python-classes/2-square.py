7bee72eda2c5bb84f1e98cc42cba49583e122f9c
WambuaJoe <kaytujohn254@gmail.com>
2023-10-03 22:39:47 +0300

    code update - validate private instance attribute 'size'

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,12 +7,8 @@ class Square:
     defines a Square & its basic properties
     """
     def __init__(self, size=0):
-        try:
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-        except Exception as err:
-            print(err)
-        else:
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

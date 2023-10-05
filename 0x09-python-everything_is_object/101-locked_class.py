d3a7a1b5d987dac094f04824ec441829551820ed
WambuaJoe <kaytujohn254@gmail.com>
2023-10-05 13:10:04 +0300

    implement __slots__ to allocate space for a fixed amount of variables

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """ LockedClass module """
+
+
+class LockedClass:
+    """ prevent user from dynamically creating new instance
+        attributes, except if the new instance is first_name
+    """
+    __slots__ = ['first_name']

c4b38905b579e45759d47cd7e8f49bf767c65756
WambuaJoe <kaytujohn254@gmail.com>
2023-10-12 11:07:04 +0300

    code fix - append string at EOF

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """ append string at the end of a text file """
 
     with open(filename, 'a+', encoding='UTF-8') as file:
-        file.write(text)
+        return file.write(text)

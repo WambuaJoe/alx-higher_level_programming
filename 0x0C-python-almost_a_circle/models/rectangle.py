a1123003a6af5e6c77ee3e8a66295defba125441
WambuaJoe <kaytujohn254@gmail.com>
2023-10-19 12:01:43 +0300

    code update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+#!/bin/python3
 """ Rectangle module """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ class Rectangle that inherits from Base class """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ initialize width, height, x, y """
+    """ create Rectangle class, that inherits from Base class """
+    def __init__(self, width, height, x=0, y=0):
+        """ initialize rectangle width, height, x, y """
         self.width = width
         self.height = height
         self.x = x
@@ -15,50 +15,35 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ return rectangle width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ set rectangle width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
+        """ return rectangle height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ set rectangle height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     @property
     def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be > 0")
-        else:
-            self.__x
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be > 0")
-        else:
-            self.__y
+        """ return x """
+        pass

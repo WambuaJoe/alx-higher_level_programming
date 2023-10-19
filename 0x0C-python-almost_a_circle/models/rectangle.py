6481112a18f64c1587be04a7bf0a38d87af09217
WambuaJoe <kaytujohn254@gmail.com>
2023-10-19 21:55:02 +0300

    implement str representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,9 @@ class Rectangle(Base):
             print("")
         for a in range(self.__height):
             print("" * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """ implement str representation """
+        print("[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, super().id,
+                                               self.__x, self.__y,
+                                               self.__width, self.__height))

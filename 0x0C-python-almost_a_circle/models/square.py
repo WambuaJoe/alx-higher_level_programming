151d4517387461353ca77550c6b1824ef04b1b10
WambuaJoe <kaytujohn254@gmail.com>
2023-10-23 08:44:22 +0300

    code fix - update getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,7 +17,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """ return square size """
-        return self.size
+        return self.width
 
     @size.setter
     def size(self, value):

1968bc768a663dd5de56c9aa82a16de62a409ac0
WambuaJoe <kaytujohn254@gmail.com>
2023-11-27 22:44:07 +0300

    create 2nd table in DB; add records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- create 2nd table in DB
+-- add multiple rows
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table VALUE (1, "John", 10);
+INSERT INTO second_table VALUE (2, "Alex", 3);
+INSERT INTO second_table VALUE (3, "Bob", 14);
+INSERT INTO second_table VALUE (4, "George", 8);

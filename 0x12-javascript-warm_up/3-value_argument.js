e12e8a0489d05f65ec090681676418396acb23d1
WambuaJoe <kaytujohn254@gmail.com>
2023-12-18 15:46:55 +0300

    code update - fix linter error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
+if (args === 0) {
   console.log('No argument');
 } else {
-  console.log(' ' +args);
+  console.log(' ' + args);
 }

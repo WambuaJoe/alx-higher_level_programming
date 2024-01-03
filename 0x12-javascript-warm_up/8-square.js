f2975ad40a5ff9bf8e07d00fcc45f334216daa03
WambuaJoe <kaytujohn254@gmail.com>
2024-01-03 08:54:08 +0300

    code update - fix indentation errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,13 +4,13 @@
 const count = parseInt(process.argv[2]);
 
 if (isNaN(count)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let index = 0; index < count; index++) {
-        let row = '';
-        for (let j = 0; j < count; j++) {
-            row += 'X';            
-        }
-        console.log(row);
+  for (let index = 0; index < count; index++) {
+    let row = '';
+    for (let j = 0; j < count; j++) {
+      row += 'X';
     }
+    console.log(row);
+  }
 }

cbf0c4b10b3a3c0f6d0241cc665c3151a339333c
WambuaJoe <kaytujohn254@gmail.com>
2024-01-03 09:28:15 +0300

    code update - semistandard error fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // print result of addition of 2 integers
 function add (a, b) {
-    const sum = a + b;
-    console.log(sum);
+  const sum = a + b;
+  console.log(sum);
 }
 
 const valInt1 = Number(process.argv[2]);
 const valInt2 = Number(process.argv[3]);
 
 if (Number.isNaN(valInt1) || Number.isNaN(valInt2)) {
-    console.log('Nan');
+  console.log('Nan');
 } else {
-    add(valInt1, valInt2);
+  add(valInt1, valInt2);
 }

f7129b9276cfbf288bff35c353f827e18f4ce954
WambuaJoe <kaytujohn254@gmail.com>
2024-01-30 18:32:53 +0300

    code update - fix semistandard error

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // function that increments & calls a function
 exports.addMeMaybe = function addMeMaybe (number, theFunction) {
-    return theFunction(++number);
-}
+  return theFunction(++number);
+};

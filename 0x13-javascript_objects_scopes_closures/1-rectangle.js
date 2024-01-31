8d719c0f3677128266c67e3c20e4282d40dc77b4
WambuaJoe <kaytujohn254@gmail.com>
2024-02-01 01:15:58 +0300

    code update - fix semistandard linter error

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // create Rectangle class that defines a rectangle
 class Rectangle {
-    w;
-    h;
+  w;
+  h;
 
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;

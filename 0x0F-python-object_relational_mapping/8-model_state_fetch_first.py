d02a4992ed97ce25ec7e2e24ddd61ba38744d966
WambuaJoe <kaytujohn254@gmail.com>
2024-02-13 18:24:19 +0300

    code update - fix output & documentation

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
 """
-Prints first State object from database hbtn_0e_6_usa.
-Usage: ./8-model_state_fetch_first.py <mysql username> /
-                                      <mysql password> /
-                                      <database name>
+list first State object from database
 """
-import sys
+
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-
-# FUNCTIONALITY
-# 1. Importing the necessary modules
-# 2. Creating the engine and session
-# 3. Querying the database for the first State object
-# 4. Printing the id and name of the first State object
-# 5. If there is no State object, print “Nothing”
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
-
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
+    """print the first State object the database hbtn_0e_6_usa
+    """
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state is not None:
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("Nothing")
+    session.close()

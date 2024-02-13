afa43272f5ba6be23e82cd52dff022e52936dd76
WambuaJoe <kaytujohn254@gmail.com>
2024-02-13 20:37:40 +0300

    take a letter & send POST request to URL with letter as parameter

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    url = sys.argv[1]
+    # url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))

a6a39738446822e9148f50fd7655bf573f3d7fcd
WambuaJoe <kaytujohn254@gmail.com>
2024-03-21 14:12:42 +0300

    convert number from base 10 to another passed as an arg

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// converts a number from base 10 to another base passed as argument
+
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((x, xI) => x * xI));

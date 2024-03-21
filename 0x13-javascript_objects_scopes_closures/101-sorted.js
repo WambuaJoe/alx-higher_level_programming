fd411e2935d8565b70066918c6f86af9dbce46f8
WambuaJoe <kaytujohn254@gmail.com>
2024-03-21 14:38:33 +0300

    concatenate 2 files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that imports a dictionary of occurrences by user id
 // and computes a dictionary of user ids by occurrence.
-let dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }

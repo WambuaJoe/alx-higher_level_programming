c3148537b415032fb260267ee4a95882d0e6d708
WambuaJoe <kaytujohn254@gmail.com>
2024-03-21 14:15:31 +0300

    code update - fix linter erros

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // converts a number from base 10 to another base passed as argument
 exports.converter = function (base) {
-    function conv (number) {
-      return number.toString(base);
-    }
-    return conv;
-  };
+  function conv (number) {
+    return number.toString(base);
+  }
+  return conv;
+};

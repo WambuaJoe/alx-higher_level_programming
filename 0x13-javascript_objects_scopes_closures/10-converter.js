46668ee0be4e93557d18f15ac4433445d9ccf292
WambuaJoe <kaytujohn254@gmail.com>
2024-03-21 14:14:22 +0300

    code update - fix requirement checks

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // converts a number from base 10 to another base passed as argument
-
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((x, xI) => x * xI));
+exports.converter = function (base) {
+    function conv (number) {
+      return number.toString(base);
+    }
+    return conv;
+  };
